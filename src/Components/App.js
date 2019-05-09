286c3e42fcb7b11b032fa215c01e912867256fe2
andrewmayer <andrew@levelupdesign.com>
2019-05-09 15:36:00 -0700

    Attempting nested lists

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,13 +6,42 @@ import { Container } from 'semantic-ui-react';
 
 const SortableItem = sortableElement(({ value }) => <li>{value}</li>);
 
+const SortableGroup = sortableElement(({ value, tasks }) => {
+  return (
+    <div>
+      <li>{value.name}</li>
+      <Container text>
+        {tasks.map((value, index) => (
+          <SortableItem
+            key={`project-${index}`}
+            index={index}
+            value={value}
+            variant="primary"
+          />
+        ))}
+      </Container>
+    </div>
+  );
+});
+
 const SortableContainer = sortableContainer(({ children }) => {
-  return <ul>{children}</ul>;
+  return <ol>{children}</ol>;
 });
 
 class App extends Component {
   state = {
-    items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6']
+    items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
+    projects: [
+      {
+        name: 'Name Project',
+        tasks: ['Bob', 'Bill', 'Sarah', 'Doug', 'Kenny']
+      },
+      { name: 'Pet Project', tasks: ['Cat', 'Bird', 'Mouse', 'Goat', 'Dog'] },
+      {
+        name: 'Tool Project',
+        tasks: ['Hammer', 'Screwdriver', 'Wrench', 'Drill', 'Saw']
+      }
+    ]
   };
 
   onSortEnd = ({ oldIndex, newIndex }) => {
@@ -21,22 +50,38 @@ class App extends Component {
     }));
   };
 
+  onSectionSortEnd = (sectionIndex, { oldIndex, newIndex }) => {
+    let copiedSections = Object.assign([], this.state.sections);
+    let section = copiedSections[sectionIndex];
+    section.items = arrayMove(section.items, oldIndex, newIndex);
+
+    this.setState({
+      sections: copiedSections
+    });
+  };
+
   render() {
-    const { items } = this.state;
+    const { projects } = this.state;
 
     return (
-      <SortableContainer onSortEnd={this.onSortEnd}>
-        <Container>
-          {items.map((value, index) => (
-            <SortableItem
-              key={`item-`}
-              index={index}
-              value={value}
-              variant="primary"
-            />
-          ))}
-        </Container>
-      </SortableContainer>
+      <div>
+        <SortableContainer onSortEnd={this.onSortEnd}>
+          <Container>
+            {projects.map((value, index) => (
+              <SortableGroup
+                key={`project-`}
+                index={index}
+                value={value}
+                variant="primary"
+                tasks={value.tasks}
+                lockAxis="y"
+                onSortEnd={this.onSectionSortEnd.bind(this, index)}
+                lockToContainerEdges
+              />
+            ))}
+          </Container>
+        </SortableContainer>
+      </div>
     );
   }
 }
